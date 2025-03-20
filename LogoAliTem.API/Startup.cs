d6fcc0be40ede540eb69df0085870e5afa4578a2
Renato Fiorezze Silva <renato.fsilva@parceiro.mrv.com.br>
2025-03-20 12:10:12 -0300

    Reversao cors

--- LogoAliTem.API/Startup.cs
+++ LogoAliTem.API/Startup.cs
@@ -10,7 +10,6 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System;
@@ -31,8 +30,8 @@ public class Startup
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
-        string connectionString = Configuration.GetConnectionString("Default"); 
-        
+        string connectionString = Configuration.GetConnectionString("Default");
+
         services.AddDbContext<LogoAliTemContext>(
             context => context.UseNpgsql(connectionString)
         );
@@ -54,22 +53,16 @@ public class Startup
               .AddDefaultTokenProviders();
 
         services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-            .AddJwtBearer(options =>
-            {
-                options.RequireHttpsMetadata = true;
-                options.SaveToken = true;
-                options.TokenValidationParameters = new TokenValidationParameters
+                .AddJwtBearer(options =>
                 {
-                    ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["TokenKey"])),
-                    ValidateIssuer = true,
-                    ValidIssuer = Configuration["Jwt:Issuer"], // Define um emissor válido
-                    ValidateAudience = true,
-                    ValidAudience = Configuration["Jwt:Audience"], // Define uma audiência válida
-                    ValidateLifetime = true, // Expiração do token
-                    ClockSkew = TimeSpan.Zero // Sem tolerância para tokens expirados
-                };
-            });
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["TokenKey"])),
+                        ValidateIssuer = false,
+                        ValidateAudience = false
+                    };
+                });
 
         services.AddControllers()
                 .AddJsonOptions(options =>
@@ -92,16 +85,7 @@ public class Startup
         services.AddScoped<ILocalizacaoService, LocalizacaoService>();
         services.AddScoped<IReboqueService, ReboqueService>();
 
-        services.AddCors(options =>
-        {
-            options.AddPolicy("AllowSpecificOrigins",
-                builder => builder
-                    .WithOrigins("https://app.logoalitem.com.br", "http://app.logoalitem.com.br", "http://localhost", "http://localhost:4200")
-                    .AllowAnyMethod()
-                    .AllowAnyHeader()
-                    .AllowCredentials()
-            );
-        });
+        services.AddCors();
         services.AddSwaggerGen(options =>
         {
             options.SwaggerDoc("v1", new OpenApiInfo { Title = "LogoAliTem.API", Version = "v1" });
@@ -139,31 +123,21 @@ public class Startup
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
-        if (!env.IsDevelopment())
-        {
-            app.UseHsts(); // 🔹 Força HTTPS e melhora a segurança do transporte (somente produção)
-            app.UseHttpsRedirection();
-        }
-
         app.UseDeveloperExceptionPage();
         app.UseSwagger();
         app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "LogoAliTem.API v1"));
 
         app.UseRouting();
 
-        app.Use(async (context, next) =>
-        {
-            context.Response.Headers.Add("X-Content-Type-Options", "nosniff"); // Protege contra MIME sniffing
-            context.Response.Headers.Add("Referrer-Policy", "no-referrer"); // Evita exposição de referrers
-            context.Response.Headers.Add("X-Frame-Options", "DENY"); // Bloqueia ataques de Clickjacking
-            context.Response.Headers.Add("X-XSS-Protection", "1; mode=block"); // Protege contra XSS
-            await next();
-        });
-
-        app.UseCors("AllowSpecificOrigins");
-
         app.UseAuthentication();
         app.UseAuthorization();
+
+        app.UseCors(x => x
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+
+        app.UseHttpsRedirection();
 
         app.UseEndpoints(endpoints =>
         {
